aa9544c7b8d34e4f1780ad0b8ce102c96eba2032
d_ivahnencko <ivahnencko01@gmail.com>
2021-10-11 18:28:28 +0300

    add others

---
+++ lab_03/lib/__init__.py
@@ -0,0 +1,8 @@
+from .types import *
+from .funcs import *
+
+func_description: dict[bubble_sort, str] = {
+    bubble_sort: 'Пузырьком',
+    insertion_sort: 'Вставками',
+    selection_sort: 'Выбором',
+}
